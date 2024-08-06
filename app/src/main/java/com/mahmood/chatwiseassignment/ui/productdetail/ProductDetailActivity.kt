70ab9b2a83633758e79c4d164493a7139d304f31
Mahmood Ahmad Khan <mycode7408@gmail.com>
2024-08-06 21:41:34 +0530

    fixed issue

--- app/src/main/java/com/mahmood/chatwiseassignment/ui/productdetail/ProductDetailActivity.kt
+++ app/src/main/java/com/mahmood/chatwiseassignment/ui/productdetail/ProductDetailActivity.kt
@@ -3,6 +3,7 @@ package com.mahmood.chatwiseassignment.ui.productdetail
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.mahmood.chatwiseassignment.R
@@ -15,10 +16,15 @@ class ProductDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.product_details)
 
-        val product = intent.getSerializableExtra("product") as Product
+        val product = intent.getParcelableExtra<Product>("product")
 
-        findViewById<TextView>(R.id.textTitle).text = product.title
-        findViewById<TextView>(R.id.textDescription).text = product.description
-        Glide.with(this).load(product.thumbnail).into(findViewById(R.id.imageView))
+        if (product != null) {
+            findViewById<TextView>(R.id.textTitle).text = product.title
+            findViewById<TextView>(R.id.textDescription).text = product.description
+            Glide.with(this).load(product.thumbnail).into(findViewById(R.id.imageView))
+        } else {
+            Toast.makeText(this, "Product data is missing", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
--- app/src/main/java/com/mahmood/data/model/Product.kt
+++ app/src/main/java/com/mahmood/data/model/Product.kt
@@ -1,14 +1,40 @@
 package com.mahmood.data.model
 
 
+import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-@Parcelize
+
 data class Product(
-    val id: Int,
     val title: String,
     val description: String,
-    val price: Double,
     val thumbnail: String
-) : Parcelable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(thumbnail)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Product> {
+        override fun createFromParcel(parcel: Parcel): Product {
+            return Product(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Product?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
